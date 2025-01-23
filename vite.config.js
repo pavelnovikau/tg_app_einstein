ebaca5ddd15aa65d1f2801af64769b26cc53659a
pavelnovikau <pavel@novikau.com>
2025-01-23 16:01:39 +0100

    pages 11

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tg_app_einstein/', // Проверьте, что это точное имя вашего репозитория
+  base: '/tg_app_einstein/',
   build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-      output: {
-        manualChunks: undefined,
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      },
-    },
+    outDir: 'dist'
   },
   resolve: {
     alias: {
