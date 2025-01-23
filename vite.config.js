8ab7b0467c89e14b3329d7122f2186a24614fda6
pavelnovikau <pavel@novikau.com>
2025-01-23 15:36:00 +0100

    pages 3

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tg_app_einstein/', // Имя вашего репозитория
+  base: '/tg_app_einstein/', // Проверьте, что это точное имя вашего репозитория
   build: {
     outDir: 'dist',
     sourcemap: true
