07fd33275fc6ddde5f2eea1d8626667ee48ce6db
pavelnovikau <pavel@novikau.com>
2025-01-23 16:04:10 +0100

    pages 12

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/tg_app_einstein/',
-  build: {
-    outDir: 'dist'
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  },
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
-    loader: 'jsx',
-  },
 }) 
