e944a9175567d354040f8013fe4e4b61e334f711
pavelnovikau <pavel@novikau.com>
2025-01-24 09:46:15 +0100

    css 15

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tg_app_einstein/',
   build: {
     outDir: 'dist'
   }
